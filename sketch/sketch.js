ab7bbc54390b39d204fb7c083f68684e74ff163f
agomez12 <andreaxing@gmail.com>
2020-06-18 16:07:24 -0400

    background sketch positioned

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,11 +1,16 @@
+var canvas;
 function setup() {
-    var canvas = createCanvas(windowWidth, windowHeight);
+    canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent("sketch");
+    canvas.position(0, 0);
     frameRate(60);
 }
 
 function draw() {
     background(0, 150, 255);
+    fill(0, 0, 255);
+    rect(0, 0, 100, 100);
+    rect(0, windowHeight - 10, 100, 100);
 }
 
 function windowResized() {
