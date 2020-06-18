77493ca3a43655516b6cf7e85ad9249303e877b0
agomez12 <andreaxing@gmail.com>
2020-06-18 19:22:08 -0400

    Parallax and speed change on scroll

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -3,14 +3,41 @@ function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent("sketch");
     canvas.position(0, 0);
-    frameRate(60);
+    frameRate(30);
 }
 
+var rectX = 0
+var velX = 3
+let rightX = true
+
+let scrollChange = false
+let scroll = this.scrollY;
 function draw() {
     background(0, 150, 255);
+    translate(0, -0.25*this.scrollY);
     fill(0, 0, 255);
-    rect(0, 0, 100, 100);
-    rect(0, windowHeight - 10, 100, 100);
+    rect(rectX, windowHeight/2, 100, 100);
+    if (rectX < 0) {
+        rightX = true
+    } else if (rectX > windowWidth - 100) {
+        rightX = false
+    }
+    if (rightX) {
+        rectX += velX;
+    } else {
+        rectX -= velX;
+    }
+
+    if (scrollChange) {
+        velX = 10;
+    } else { velX = 3}
+
+    if (scroll != this.scrollY) {
+        scrollChange = true
+    } else {scrollChange = false}
+    scroll = this.scrollY;
+    console.log(scrollChange)
+    
 }
 
 function windowResized() {
