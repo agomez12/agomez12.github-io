c51000a9ba1920c52b872faaf432d3d2fae48636
agomez12 <andreaxing@gmail.com>
2020-06-21 22:55:43 -0400

    floating balls

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,9 +1,14 @@
+new p5();
 var canvas;
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent("sketch");
     canvas.position(0, 0);
     frameRate(30);
+
+    for (i = 0; i < 50; i++) {
+        particles.push(new Particle(random(0, windowWidth), random(0, windowHeight)))
+    } 
 }
 
 var rectX = 0
@@ -12,9 +17,26 @@ let rightX = true
 
 let scrollChange = false
 let scroll = this.scrollY;
+let speeds = [1, 3];
+let speed = speeds[0];
+
+let birthRate = 800
+let lastBirth = 0
+
+let particles = []
+
 function draw() {
     background(0, 150, 255);
     translate(0, -0.25*this.scrollY);
+
+    scrollSpeedChange()
+    
+    for (i of particles) {
+        i.display()
+    }
+}
+
+function testRect() {
     fill(0, 0, 255);
     rect(rectX, windowHeight/2, 100, 100);
     if (rectX < 0) {
@@ -23,23 +45,60 @@ function draw() {
         rightX = false
     }
     if (rightX) {
-        rectX += velX;
+        rectX += velX*speed;
     } else {
-        rectX -= velX;
+        rectX -= velX*speed;
     }
+}
 
+function scrollSpeedChange() {
     if (scrollChange) {
-        velX = 10;
-    } else { velX = 3}
+        speed = speeds[1];
+    } else { speed = speeds[0]}
 
     if (scroll != this.scrollY) {
         scrollChange = true
     } else {scrollChange = false}
     scroll = this.scrollY;
-    console.log(scrollChange)
-    
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+}
+
+class Particle {
+    constructor(x, y) {
+         this.pos = createVector(x, y)
+         this.vel = createVector(random(-1, 1), random(-1, 1))
+         this.size = random(10, 20)
+    }
+
+    display() {
+        push()
+        translate(this.pos.x, this.pos.y)
+        fill(245, 179, 66)
+        ellipse(0, 0, this.size, this.size)
+        pop()
+        this.move()
+        this.limits()
+    }
+
+    move() {
+        this.pos.add(this.vel)
+
+    }
+
+    limits() {
+        if (this.pos.x > windowWidth) {
+            this.pos.x = 0
+        } else if (this.pos.x < 0) {
+            this.pos.x = windowWidth
+        }
+        if (this.pos.y > windowHeight) {
+            this.pos.y = 0
+        } else if (this.pos.y < 0) {
+            this.pos.y = windowHeight
+        }
+        this.vel.setMag(random(1, 4))
+    }
 }
